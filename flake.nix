e8d051f41b84d8d979e0d515193a14c8c6fcd530
Pottarr <pottarrpongs@gmail.com>
2026-01-31 09:31:04 +0700

    Added flake.nix for NixOS Users

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+    description = "Pottarr TMXDS : TMUX Session and Directory Manager";
+
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    outputs = { self, nixpkgs }:
+    let
+        systems = [ "x86_64-linux" "aarch64-linux" ];
+        forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+        packages = forAllSystems (system:
+        let
+            pkgs = import nixpkgs { inherit system; };
+        in {
+            default = pkgs.stdenvNoCC.mkDerivation {
+            pname = "tmxds";
+            version = "1.0.0";
+
+            src = self;
+
+            buildInput = [
+                pkgs.ripgrep
+                pkgs.skim
+                pkgs.tmux
+            ];
+
+            dontBuild = true;
+
+            installPhase = ''
+                mkdir -p $out/bin
+                install -m755 tmxd tmxs $out/bin/
+            '';
+            };
+        });
+    };
+}
